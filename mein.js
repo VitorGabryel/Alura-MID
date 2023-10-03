d046de054cdfcda4d37f4daf372539fbdb1a891d
VitorGabryel <128511106+VitorGabryel@users.noreply.github.com>
2023-10-03 15:41:17 -0300

    Add files via upload

--- mein.js
+++ mein.js
@@ -1,3 +1,15 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
 document.querySelector('.tecla_pom');
+//document.querySelector('.tecla_pom').onclick = toca
 
-document.querySelector('#som_tecla_pom').play('');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+ const listaDeTeclas = document.querySelectorAll(',tecla');
+ listaDeTeclas[0].onclick = tocaSomPom;
+ listaDeTeclas[1].onclick = tocaSomClap;
+ listaDeTeclas[2].onclick = tocaSomTim;
+ listaDeTeclas[3].onclick = tocaSomFuff;
+ listaDeTeclas[4].onclick = tocaSomSplash;
+ listaDeTeclas[5].onclick = tocaSomToim;
+ listaDeTeclas[6].onclick = tocaSmoPsh;
